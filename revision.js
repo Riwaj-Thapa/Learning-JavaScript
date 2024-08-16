99648f8d64bc2e2beeefe55d74325e503c00e7c8
Riwaj Thapa <rambathapa562@gmail.com>
2024-08-16 23:12:04 +0545

    This is the revision section

---
+++ revision.js
@@ -0,0 +1,29 @@
+// for loops 
+
+console.log("This is for loop")
+for(let i = 1; i<=8;i++){
+    console.log(`the value is : ${i}`);
+
+}
+
+// while loops
+
+console.log("This is while loop")
+let i = 1;
+while(i<=8){
+    console.log(`the value is : ${i}`)
+    i++;
+}
+
+//do while loops
+
+console.log("This is do while loop")
+let a = 1;
+do{
+    console.log(`the value is : ${a}`);
+    a++;
+
+}while(a<=8);
+
+//for of loops and for in loops
+
