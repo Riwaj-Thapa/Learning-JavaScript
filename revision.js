fc7dd8435e66476af82d34e0d84bb4bed831344a
Riwaj Thapa <rambathapa562@gmail.com>
2024-08-18 23:15:53 +0545

    updating files

--- revision.js
+++ revision.js
@@ -1,29 +1,52 @@
-// for loops 
+// // // for loops 
 
-console.log("This is for loop")
-for(let i = 1; i<=8;i++){
-    console.log(`the value is : ${i}`);
+// // console.log("This is for loop")
+// // for(let i = 1; i<=8;i++){
+// //     console.log(`the value is : ${i}`);
 
-}
+// // }
 
-// while loops
+// // // while loops
 
-console.log("This is while loop")
-let i = 1;
-while(i<=8){
-    console.log(`the value is : ${i}`)
-    i++;
-}
+// // console.log("This is while loop")
+// // let i = 1;
+// // while(i<=8){
+// //     console.log(`the value is : ${i}`)
+// //     i++;
+// // }
 
-//do while loops
+// // //do while loops
 
-console.log("This is do while loop")
-let a = 1;
-do{
-    console.log(`the value is : ${a}`);
-    a++;
+// // console.log("This is do while loop")
+// // let a = 1;
+// // do{
+// //     console.log(`the value is : ${a}`);
+// //     a++;
 
-}while(a<=8);
+// // }while(a<=8);
 
-//for of loops and for in loops
+// //for of loops 
+
+// let str = "Riwaj";
+
+// for(let i of str){
+//     console.log(i);                 
+
+// }
+
+// //for in loops
+
+// const profile = {
+//     name: 'Riwaj',
+//     age: 21,
+//     height: 171
+// }
+
+// for(let i in profile){
+//         console.log(i)
+// }
+
+
+
+
 
